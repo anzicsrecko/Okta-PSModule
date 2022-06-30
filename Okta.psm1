0d09a1b3c2c9b3b77fd9af7e9f3c22a4e4bdec6f
anzicsrecko <srecko.anzic@pagaya.com>
2022-06-30 14:44:44 -0400

    edit okta

--- Okta.psm1
+++ Okta.psm1
@@ -1765,32 +1765,6 @@ function oktaConvertIndividualUsersbyAppId()
     return $request
 }
 
-function oktaAppLoginsByUserId()
-{
-    param
-    (
-        [parameter(Mandatory=$false)][ValidateLength(1,100)][String]$oOrg=$oktaDefOrg,
-        [parameter(Mandatory=$true)][ValidateLength(20,20)][String]$aid,
-        [parameter(Mandatory=$true)][ValidateLength(20,20)][String]$uid
-    )
-    [string]$method = "Get"
-    #[string]$resource = '/api/v1/logs?filter=eventType eq "user.authentication.sso" and outcome.result eq "SUCCESS" and actor.id eq "' + $uid + '" and target.id eq "' + $aid + '"'
-    [string]$resource = '/api/v1/logs?filter=eventType eq "user.authentication.sso"'
-    try
-    {
-        $request = _oktaNewCall -method $method -resource $resource -oOrg $oOrg
-    }
-    catch
-    {
-        if ($oktaVerbose -eq $true)
-        {
-            Write-Host -ForegroundColor red -BackgroundColor white $_.TargetObject
-        }
-        throw $_
-    }
-    return $request
-}
-
 function oktaListUsers()
 {
     param
